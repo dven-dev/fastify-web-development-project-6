2ed35601903b447bf24cbfc68fe6e2ed9a717639
Dominika Vender <me@dven.dev>
2026-07-09 01:10:24 +0300

    Add temporary test route for Rollbar

--- server/routes/welcome.js
+++ server/routes/welcome.js
@@ -4,6 +4,9 @@ export default (app) => {
     .get('/', { name: 'root' }, (req, reply) => {
       reply.render('welcome/index');
     })
+    .get('/test-rollbar', async (req, reply) => {
+      throw new Error('Test Rollbar error');
+    })
     .get('/protected', { name: 'protected', preValidation: app.authenticate }, (req, reply) => {
       reply.render('welcome/index');
     });
