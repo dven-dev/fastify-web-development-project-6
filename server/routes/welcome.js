72a00fdd4d502bff61314ed1c7190e8eadb8a584
Dominika Vender <me@dven.dev>
2026-07-09 01:22:49 +0300

    Remove test rollbar route

--- server/routes/welcome.js
+++ server/routes/welcome.js
@@ -4,9 +4,6 @@ export default (app) => {
     .get('/', { name: 'root' }, (req, reply) => {
       reply.render('welcome/index');
     })
-    .get('/test-rollbar', async (req, reply) => {
-      throw new Error('Test Rollbar error');
-    })
     .get('/protected', { name: 'protected', preValidation: app.authenticate }, (req, reply) => {
       reply.render('welcome/index');
     });
