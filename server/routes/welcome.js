614b923886eba9f7478662c9ca4f2b190aa4403a
Dominika Vender <me@dven.dev>
2026-07-09 00:57:37 +0300

    Add Rollbar test route

--- server/routes/welcome.js
+++ server/routes/welcome.js
@@ -5,6 +5,7 @@ export default (app) => {
     .get('/', { name: 'root' }, (req, reply) => {
       reply.render('welcome/index');
     })
+    app.get('/test-error', async (req, reply) => { throw new Error('Test Rollbar error'); });
     .get('/protected', { name: 'protected', preValidation: app.authenticate }, (req, reply) => {
       reply.render('welcome/index');
     });
