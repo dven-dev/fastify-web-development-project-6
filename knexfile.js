031e13ffaf554523c7e0a32bc66b0a9cadb66a9e
Dominika Vender <me@dven.dev>
2026-07-09 12:28:12 +0300

    Add PostgreSQL support for production

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,4 @@
 // @ts-check
-
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -22,15 +21,11 @@ export const test = {
   client: 'sqlite3',
   connection: ':memory:',
   useNullAsDefault: true,
-  // debug: true,
   migrations,
 };
 
 export const production = {
-  client: 'sqlite3',
-  connection: {
-    filename: path.resolve(__dirname, 'database.sqlite'),
-  },
-  useNullAsDefault: true,
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
   migrations,
 };
